680148676444a237e8cb1f3065f436f5aa4291c6
calvinxxl <51313106+calvinxxl@users.noreply.github.com>
2024-10-17 15:42:19 +0800

    Update test-c.py

--- Test Records/test-c.py
+++ Test Records/test-c.py
@@ -9,4 +9,4 @@ import os
 #UDP TEST
 os.system('sudo ifconfig eth0 195.20.1.31')
 os.system('sudo ifconfig eth0')
-os.system('sudo iperf3 -c 195.20.1.19 -b 100M -n 50G -u -t 5')
+os.system('sudo iperf3 -c 195.20.1.19 -b 100M -n 50G -u -i 5')
